00dc063ec64ed8abb5ea6a432ad607f29fdfdc77
Paulo Cesar <paulo.souza@kryptus.com>
2021-07-17 15:07:50 -0300

    Refactor code and and endpoint to post a new message

--- vMail/server/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Message(models.Model):
-    subject = models.CharField(max_length=200)
-    content = models.CharField(max_length=1000)
-    startDate = models.DateTimeField()
-    expirationDate = models.DateTimeField()
--- vMail/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('all', views.allMessages, name='all'),
-    path('<int:MessageID>/', views.messageDetails, name='details'),
-]
